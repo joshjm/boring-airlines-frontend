e7c61b14b9e1ccdadacc294cb77469dd1ea9e855
Joshua McDonald <mcdojj12@gmail.com>
2020-07-09 10:24:46 +1000

    cosmetic changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import './Home.css'
 export default function Home(){
     return(
-        <h2>home diggetys</h2>
+        <div>
+            <h1>Welcome!</h1>
+            <h2> Let Boring Airlines be your first choice to transport you nowhere.</h2>
+            
+            <img src='https://thehustle.co/wp-content/uploads/2019/11/header-1-1.gif'/>
+       </div>
     )
 }
